519eeb56fa5c4ad1e395ca64c1da77d9f0aa52c0
Elran Shefer <elran777@gmail.com>
2019-10-19 04:08:26 +0300

    fixed crud

--- app/backend/server/core/crud.py
+++ app/backend/server/core/crud.py
@@ -1,23 +1,25 @@
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 from models import *  # pylint: disable=wildcard-import, unused-wildcard-import
 
-username = input("enter db username")
-password = input("enter db password")
-host = input("enter db host")
+username = os.environ.get('ONE_REPORT_USERNAME', 'one_report')
+password = os.environ.get('ONE_REPORT_PASSWORD', 'one_report')
+host = os.environ.get('ONE_REPORT_HOST', 'localhost')
 DATABASE_URI = f'postgres+psycopg2://{username}:{password}@{host}:5432/one_report'
 
 engine = create_engine(DATABASE_URI)
 Session = sessionmaker(bind=engine)
 s = Session()
 
+
 def recreate_database():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     mador = Mador(name='pie')
-    mador_settings = MadorSettings(mador=mador, key='default_reminder_time', value='08:00', type='time')
+    mador_settings = MadorSettings(
+        mador=mador, key='default_reminder_time', value='08:00', type='time')
 
     s.add_all([mador, mador_settings])
     s.commit()
-
