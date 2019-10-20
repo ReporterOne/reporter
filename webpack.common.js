ca7a7ce8078e2ac590bf4143a81dd8a47e95a08f
Elran Shefer <elran777@gmail.com>
2019-10-20 16:55:33 +0300

    fixed git ignore

--- webpack.common.js
+++ webpack.common.js
@@ -37,6 +37,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 loader: "babel-loader",
+                exclude: /node_modules/,
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
