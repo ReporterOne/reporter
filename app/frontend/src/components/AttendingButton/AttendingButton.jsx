0455b4af6678be4f252d959e6d3f879751d065ce
ddomb <domb200@gmail.com>
2019-12-13 22:58:00 +0200

    pushing so elran can work from his pc.

--- app/frontend/src/components/AttendingButton/AttendingButton.jsx
+++ app/frontend/src/components/AttendingButton/AttendingButton.jsx
@@ -1,4 +1,5 @@
-import React, {useState, useCallback} from "react";
+import React, { useState, useCallback, useRef } from "react";
+import { AutoSizer } from 'react-virtualized';
 
 import styled from 'styled-components';
 import Arrows from '~/assets/Arrows.svg';
@@ -10,7 +11,7 @@ import {
 } from '~/components/common';
 
 import posed from 'react-pose';
-import  {spring} from "popmotion";
+import { spring } from "popmotion";
 
 const ContainerHeight = 50;
 const windowSize = window.innerWidth;
@@ -32,28 +33,33 @@ const PosedRRoundedRectangle = posed.div({
 const PosedCircle = posed.div({
   draggable: 'x',
   notHere: {
-    x: -(windowSize/2-rectangleMargin-(circleDiameter/2)-3)
+    x: ({ containerWidth }) => {
+      console.log(containerWidth);
+      return -(containerWidth / 2 - (circleDiameter / 2) - 3)
+    }
   },
   here: {
-    x: windowSize/2-rectangleMargin-(circleDiameter/2) -3,
+    x: ({ containerWidth }) => {
+      console.log(containerWidth);
+      return containerWidth / 2 - (circleDiameter / 2) - 3
+    }
   },
   notDecided: {
     x: 0
   },
   dragEnd: {
-    transition: ({ from, to, velocity }) => 
+    transition: ({ from, to, velocity }) =>
       spring({ from, to, velocity, stiffness: 750, damping: 50 })
   },
-  dragBounds: 
-  {
-    right: windowSize/2-rectangleMargin-(circleDiameter/2)- 3 ,
-    left: -(windowSize/2-rectangleMargin-(circleDiameter/2)) -3
-  }
+  dragBounds: ({ containerWidth }) => ({
+    right: containerWidth / 2,
+    left: -containerWidth / 2
+  })
 });
 
 const PosedArrowsContainer = posed.div({
-  decided: {scale: 0},
-  notDecided: {scale: 1}
+  decided: { scale: 0 },
+  notDecided: { scale: 1 }
 });
 
 const Container = styled.div`
@@ -71,9 +77,13 @@ const RoundedRectangle = styled(PosedRRoundedRectangle)`
   margin-right: ${rectangleMargin}px;
   flex-direction: row;
   align-items: center;
-  justify-content:center;
   border-radius: ${windowSize}px;
   ${innerShaddow[4]}
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  flex: d
 `;
 
 const Circle = styled(PosedCircle)`
@@ -117,25 +127,27 @@ const AttendenceValue = styled.span`
 
 const AttendingButton = (props) => {
 
+  const ref = useRef(null);
   const [pose, changePose] = useState('notDecided');
   const [position, changePosition] = useState(0);
   const [attendenceStatus, changeAttendenceStatus] = useState('');
   const [decided, changeDecision] = useState('notDecided');
+
   const onDragEnd = useCallback((e) => {
     const positionString = e.path[0].style.transform
-    if (positionString != 'none'){
+    if (positionString != 'none') {
       const circlePosition = Number(positionString.match(/(-?)(\d+)/)[0])
-      if(circlePosition >= ((windowSize/2-rectangleMargin-(circleDiameter/2))*0.5)) {
+      if (circlePosition >= ((windowSize / 2 - rectangleMargin - (circleDiameter / 2)) * 0.5)) {
         changePose('here');
         changeAttendenceStatus("Attending");
         changeDecision('decided');
-      } 
-      else if (circlePosition <= -((windowSize/2-rectangleMargin-(circleDiameter/2))*0.5)) {
+      }
+      else if (circlePosition <= -((windowSize / 2 - rectangleMargin - (circleDiameter / 2)) * 0.5)) {
         changePose('notHere');
         changeAttendenceStatus("Missing");
         changeDecision('decided');
       }
-      else{
+      else {
         changePose('notDecided');
         changeAttendenceStatus('');
         changeDecision('notDecided');
@@ -146,26 +158,32 @@ const AttendingButton = (props) => {
 
   return (
     <Container>
-      <RoundedRectangle poseKey={position} pose={pose}>
+      <RoundedRectangle ref={ref} poseKey={position} pose={pose}>
+        <AutoSizer>
+          {({ height, width }) => (
+            <div style={{ height, width, display: 'flex' }} className="testME">
+              <ArrowsContainer poseKey={position} pose={decided}>
+                <ArrowsLeft src={Arrows} />
+              </ArrowsContainer>
 
-        <ArrowsContainer poseKey={position} pose={decided}>
-          <ArrowsLeft src={Arrows}/>
-        </ArrowsContainer>
+              <Circle onDragEnd={onDragEnd} poseKey={position} containerWidth={width} pose={pose} />
 
-        <Circle onDragEnd={onDragEnd} poseKey={position} pose={pose}/>
+              <AttendenceValue>
+                {attendenceStatus}
+              </AttendenceValue>
 
-        <AttendenceValue>
-          {attendenceStatus}
-        </AttendenceValue>
-
-        <ArrowsContainer poseKey={position} pose={decided}>
-          <ArrowsRight src={Arrows}/>
-        </ArrowsContainer>
-
+              <ArrowsContainer poseKey={position} pose={decided}>
+                <ArrowsRight src={Arrows} />
+              </ArrowsContainer>
+            </div>
+          )}
+        </AutoSizer>
       </RoundedRectangle>
+
+
     </Container>
-        
-    
+
+
   );
 }
 
