4726aa9d8bf26cd0b236d333902fe2d7c6cf0df8
Elran Shefer <elran777@gmail.com>
2019-12-14 22:24:15 +0200

    added enter animation

--- app/frontend/src/components/Avatar/AvatarExpanded.jsx
+++ app/frontend/src/components/Avatar/AvatarExpanded.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Avatar from './Avatar.jsx';
+import posed from 'react-pose';
 
 import { Container } from '~/components/common';
 
@@ -26,13 +27,15 @@ const Background = styled(Container)`
   position: absolute;
 `;
 
-const Details = styled(Container)`
+const Details = styled.div`
   border-top-right-radius: 5px;
   border-bottom-right-radius: 5px;
   background-color: white;
   height: calc(100% - 11px - 4px);
   margin: 9px 0 2px 0;
   padding: 2px 0; 
+  overflow: hidden;
+  white-space: nowrap;
 `;
 
 const Name = styled(Container)`
@@ -40,26 +43,38 @@ const Name = styled(Container)`
   font-weight: 600;
 `;
 
-const Reason = styled(Container)`
+const Reason = styled.div`
   justify-content: center;
   align-items: center;
   font-size: 1.8rem;
   line-height: 100%;
+  display: flex;
 `;
 
+const PosedDetails = posed(Details)({
+  enter: {
+    width: "100%",
+    delay: ({innerDelay}) => innerDelay * 100
+  },
+  exit: {
+    width: "0%",
+    delay: ({innerDelay}) => innerDelay * 100
+  }
+});
 
-export const AvatarExpanded = ({name, details, ...props}) => {
+
+export const AvatarExpanded = ({name, details, delay=0, ...props}) => {
   return (
     <AvatarWrapper row>
       <Avatar type="big" {...props} />
       <Background stretched>
       </Background>
-      <Details stretched row>
+      <PosedDetails row innerDelay={delay}>
         <Container stretched>
           <Name>{name}</Name>
           <Reason>{details}</Reason>
         </Container>
-      </Details>
+      </PosedDetails>
     </AvatarWrapper>
 
   );
