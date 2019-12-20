7e079f149bf2cb4a1e618d050e2f5d977b976214
ddomb <domb200@gmail.com>
2019-12-20 16:29:34 +0200

    Added basic functionality, I am waiting for redux and basic api.

--- app/frontend/src/components/Calendar/components/Cells.jsx
+++ app/frontend/src/components/Calendar/components/Cells.jsx
@@ -8,10 +8,11 @@ import {
   endOfWeek,
   isSameMonth,
   eachWeekOfInterval,
-  eachDayOfInterval
+  eachDayOfInterval,
+  isPast,
 } from "date-fns";
 import styled from 'styled-components';
-import { Container } from '~/components/common';
+import { Container, theme } from '~/components/common';
 
 const CellsDateFormat = "d";
 
@@ -28,18 +29,50 @@ const Day = styled.div`
   display: flex;
   flex:1;
   flex-wrap: wrap;
-  border-color:red;
-  border-width:1px;
   align-items: center;
   justify-content: center;
+  background-color: ${props => dayColor(props)};
+  ${props => dayWhen[props.when]}
 `;
-
 const DateLabel = styled.span`
   line-height: 1;
   font-weight: bold;
-  color: rgb(120, 120, 120);
+  color: ${props => dayLabelIsPased(props) };
   opacity: ${props => props.isSameMonth ? 1 : 0.2};
 `;
+
+const dayColor = (props) => {
+  if (!props.isPast) {
+    return props.isSameMonth ? dayStatus[props.status] : 'rgb(255, 255, 255)'
+  }
+  else {
+    return 'rgb(255, 255, 255)'
+  }
+}
+const dayLabelIsPased = (props) => {
+  if (!props.isPast) {
+    return  props.isSameMonth ? dateLabelStatus[props.decided] : 'rgb(120, 120, 120)' 
+  }
+  else {
+    return dayStatus[props.status]
+  }
+}
+
+const dayWhen = {
+  Start: "border-top-left-radius:20px;border-bottom-left-radius:20px;",
+  Mid: "margin-top: 1px;margin-bottom: 1px;",
+  End: "border-top-right-radius:20px;border-bottom-right-radius:20px;",
+  Single: "border-radius:100px;margin: 1px;"
+};
+const dayStatus = {
+  here: theme.approved,
+  notHere: theme.notApproved,
+  notDecided: 'rgb(255, 255, 255)'
+};
+const dateLabelStatus = {
+  no: 'rgb(120, 120, 120)',
+  yes: 'rgb(255, 255, 255)'
+};
 
 const Cells = (props) => {
   const rows = useMemo(() => {
@@ -47,6 +80,7 @@ const Cells = (props) => {
     const monthEnd = endOfMonth(monthStart);
     const startDate = startOfWeek(monthStart);
     const endDate = endOfWeek(addWeeks(monthEnd, 1));
+
     return eachWeekOfInterval({
       start: startDate,
       end: endDate
@@ -59,11 +93,12 @@ const Cells = (props) => {
               end: endOfWeek(date)
             }).map(date => {
               return (
-                <Day key={date} onClick={() => props.onDateClick(date)}>
-                  <DateLabel isSameMonth={isSameMonth(date, monthStart)}>
+                <Day status={'here'} isPast={isPast(date)} when={'Mid'} isSameMonth={isSameMonth(date, monthStart)} key={date} onClick={() => props.onDateClick(date)}>
+                  <DateLabel decided={'yes'} status={'here'} isPast={isPast(date)} isSameMonth={isSameMonth(date, monthStart)}>
                     {format(date, CellsDateFormat)}
                   </DateLabel>
-                </Day>);
+                </Day>
+              );
             })
           }
         </Week>
