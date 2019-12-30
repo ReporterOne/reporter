ad889e750485b18c8fda47c1a1e2e2e4e2334314
tugy <tugmica@gmail.com>
2019-12-30 07:13:39 -0800

    fixed after merge

--- app/backend/db/crud/date_datas.py
+++ app/backend/db/crud/date_datas.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import Session
 from datetime import date, time
 from typing import List
+from utils.datetime_utils import daterange
 
 from db.models import DateData
 from db import schemas
+from .reasons import get_reason_by_name
 
 
 def get_dates_data(
@@ -12,6 +14,7 @@ def get_dates_data(
     start_date: date,
     end_date: date = None
 ) -> List[DateData]:
+
     if end_date:
         return db.query(DateData).filter(
             DateData.user_id == user_id,
@@ -29,7 +32,8 @@ def get_multiple_users_dates_data(
     users_id: List[int],
     start_date: date,
     end_date: date = None
-) -> List[schemas.DateResponse]:
+) -> List[schemas.RangeDatesResponse]:
+
     return [
         {
             'user_id': user_id,
@@ -37,3 +41,69 @@ def get_multiple_users_dates_data(
         }
         for user_id in users_id
     ]
+
+
+def set_new_date_data(
+    db: Session, 
+    body=schemas.PostDateDataBody
+) -> schemas.RangeDatesResponse:
+
+    dates_data = []
+    for date in daterange(body.start_date, body.end_date):
+        if reason is not None:
+            reason = get_reason_by_name(db, body.reason)
+
+        dates_data.append(DateData(
+                date=date, 
+                user_id=body.user_id,
+                state=body.state, 
+                reason=reason, 
+                reported_by=body.reported_by, 
+                reported_time=body.reported_time
+            ))
+    db.add_all(dates_data)
+    db.commit()
+    return {'user_id': body.user_id, 'data': dates_data}
+
+
+def delete_users_dates_data(
+    db: Session,
+    users_id: List[int],
+    start_date: date,
+    end_date: date = None
+):
+
+    for user_id in users_id:
+        dates_data_to_remove = get_dates_data(
+                db=db, 
+                user_id=user_id, 
+                start_date=start_date, 
+                end_date=end_date
+            )
+
+        for date_data in dates_data_to_remove:
+            db.delete(date_data)
+
+def put_data_in_user(
+    db: Session, 
+    body=schemas.PutDateDataBody
+) -> schemas.RangeDatesResponse:
+
+    request = body.dict()
+    if body.reason is not None:
+        request["reason"] = get_reason_by_name(db, body.reason)
+
+    dates_data = get_dates_data(
+            db=db, 
+            user_id=request.pop("user_id"),
+            start_date=request.pop("start_date"),
+            end_date=request.pop("end_date")
+        )
+
+    for datedata in dates_data:
+        for key, value in body.dict().items():
+            if value is not None:
+                setattr(datedata, key, value)
+    
+    db.commit()
+    return  {'user_id': body.user_id, 'data': dates_data}
--- app/backend/db/crud/reasons.py
+++ app/backend/db/crud/reasons.py
@@ -4,6 +4,12 @@ from typing import List
 
 from db.models import Reason
 
+def get_reason_by_name(db: Session, reason: str) -> str:
+    reason_to_return = db.query(Reason).filter(Reason.name == reason).first()
+    if reason_to_return is None:
+        raise AttributeError(f"Reason: {reason} does not exsists in the DB.")
+
+    return reason_to_return
 
 def get_reasons(db: Session):
     return [reason.name for reason in db.query(Reason).all()]
