f761f4ed69595950ef8164f46e0c373d3a17b099
Elran Shefer <elran777@gmail.com>
2020-01-04 20:02:49 +0200

    added testings for frontend

--- app/frontend/src/reducers/index.js
+++ app/frontend/src/reducers/index.js
@@ -1,3 +1,2 @@
-export {exampleReducer} from './example';
 export {generalReducer} from './general';
 export {usersReducer} from './users';
--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
-const path = require("path");
+const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 
 const frontend = path.resolve(__dirname, 'app', 'frontend');
@@ -11,14 +11,14 @@ const src = path.resolve(frontend, 'src');
 module.exports = {
   output: {
     library: 'one_report_web',
-    filename: "static/[name].js",
+    filename: 'static/[name].js',
     path: dist,
-    publicPath: '/'
+    publicPath: '/',
   },
   entry: {
     main: path.resolve(src, 'index.js'),
     avatars: path.resolve(src, 'assets', 'avatars', 'index.js'),
-    fonts: path.resolve(src, 'assets', 'fonts', 'index.js')
+    fonts: path.resolve(src, 'assets', 'fonts', 'index.js'),
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -26,62 +26,61 @@ module.exports = {
       template: path.resolve(src, 'index.html'),
     }),
     new CopyPlugin([
-      { from: path.resolve(src, 'manifest.json'), to: path.resolve(dist, 'static') }
-    ])
+      {
+        from: path.resolve(src, 'manifest.json'),
+        to: path.resolve(dist, 'static'),
+      },
+    ]),
   ],
   resolve: {
     extensions: ['.js', '.jsx'],
-    alias: {
-      "~": path.resolve(src),
-      "@": path.resolve(src, 'screens'),
-    }
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.inline.svg$/,
-        loader: 'react-svg-loader'
+        loader: 'react-svg-loader',
       },
       {
         test: /^(?!.*\.inline\.svg$).*\.svg$/,
         loader: 'svg-url-loader',
         options: {
           limit: 10000,
-          name: '/static/[name].[ext]'
-        }
+          name: '/static/[name].[ext]',
+        },
       },
       {
         test: /\.(jpe?g|png|gif)$/i,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]",
-          outputPath: "static/img/",
-          publicPath: "/static/img/"
-        }
+          name: '[name].[ext]',
+          outputPath: 'static/img/',
+          publicPath: '/static/img/',
+        },
       },
       {
         test: /\.(ttf)$/i,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]",
-          outputPath: "static/font/",
-          publicPath: "/static/font/"
-        }
+          name: '[name].[ext]',
+          outputPath: 'static/font/',
+          publicPath: '/static/font/',
+        },
       },
       {
         test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   optimization: {
     splitChunks: {
-      chunks: 'all'
-    }
-  }
-}
+      chunks: 'all',
+    },
+  },
+};
