06194f0892d4febc35a7d6214d595d6e1704d1d5
Elran Shefer <elran777@gmail.com>
2020-02-01 23:03:50 +0200

    added slide tests

--- tests/frontend/eventHelpers.js
+++ tests/frontend/eventHelpers.js
@@ -1,11 +1,31 @@
-function createClientXY(x, y) {
-  return { clientX: x, clientY: y };
+// function createClientXY(x, y) {
+//   return { clientX: x, clientY: y };
+// }
+//
+// export function createStartTouchEventObject({ x = 0, y = 0 }) {
+//   return { touches: [createClientXY(x, y)] };
+// }
+//
+// export function createMoveTouchEventObject({ x = 0, y = 0}) {
+//   return { changedTouches: [createClientXY(x, y)] };
+// }
+
+function createClientXYObject(x, y) {
+  return { clientX: x, clientY: y }
+}
+const preventDefault = () => {}
+export function createTouchEventObject({ x, y, ...rest }) {
+  return {
+    touches: [createClientXYObject(x, y)],
+    preventDefault,
+    ...rest
+  }
 }
 
-export function createStartTouchEventObject({ x = 0, y = 0 }) {
-  return { touches: [createClientXY(x, y)] };
-}
-
-export function createMoveTouchEventObject({ x = 0, y = 0}) {
-  return { changedTouches: [createClientXY(x, y)] };
+export function createMouseEventObject({ x, y, ...rest }) {
+  return {
+    ...createClientXYObject(x, y),
+    preventDefault,
+    ...rest
+  }
 }
