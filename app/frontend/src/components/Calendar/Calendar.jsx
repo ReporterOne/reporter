c0f9b0edbfa6a6cc72751cab2394f5f4e8d9c069
Elran Shefer <elran777@gmail.com>
2020-02-01 15:00:34 +0200

    changed calendar anition to be a slider

--- app/frontend/src/components/Calendar/Calendar.jsx
+++ app/frontend/src/components/Calendar/Calendar.jsx
@@ -1,26 +1,38 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useMemo} from 'react';
+import {motion, AnimatePresence} from 'framer-motion';
 import {subMonths, addMonths} from 'date-fns';
 import {Header, Days, Cells} from './components';
 import styled from 'styled-components';
 import {Swipeable} from 'react-swipeable';
-import {FadeInContainer} from '~/components/common';
+
 
 const StyledSwipeable = styled(Swipeable)`
     flex: 1;
-    display: flex;
-    flex-direction: column;
+    position: relative;
+    white-space: nowrap;
+`;
+
+const StyledContainer = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  display: inline-flex;
+  flex-direction: column;
+  position: absolute;
 `;
 
 const Calendar = ({userIdList}) => {
   const date = new Date();
   const [currentDate, setCurrentDate] = useState(date);
   const [selectedDate, setSelectedDate] = useState(date);
+  const [swipedLeft, setSwipedLeft] = useState(true);
 
   const nextMonth = useCallback((e) => {
+    setSwipedLeft(false);
     setCurrentDate(addMonths(currentDate, 1));
   });
 
   const prevMonth = useCallback((e) => {
+    setSwipedLeft(true);
     setCurrentDate(subMonths(currentDate, 1));
   });
 
@@ -28,13 +40,25 @@ const Calendar = ({userIdList}) => {
     setSelectedDate(day);
   });
 
+  const slideAmount = useMemo(() => {
+    return swipedLeft? -300 : 300;
+  }, [swipedLeft]);
+
   return (
     <StyledSwipeable onSwipedRight={prevMonth} onSwipedLeft={nextMonth}>
-      <FadeInContainer stretched poseKey={currentDate}>
-        <Header currentDate={currentDate} selectedDate={selectedDate} />
-        <Days currentDate={currentDate} />
-        <Cells currentDate={currentDate} onDateClick={onDateClick} userIdList={userIdList} />
-      </FadeInContainer>
+      <AnimatePresence>
+        <StyledContainer
+          key={currentDate}
+          initial={{x: slideAmount, opacity: 0}}
+          animate={{x: 0, opacity: 1}}
+          exit={{x: slideAmount * -1, opacity: 0}}
+        >
+          <Header currentDate={currentDate} selectedDate={selectedDate}/>
+          <Days currentDate={currentDate}/>
+          <Cells currentDate={currentDate} onDateClick={onDateClick}
+                 userIdList={userIdList}/>
+        </StyledContainer>
+      </AnimatePresence>
     </StyledSwipeable>
 
   );
